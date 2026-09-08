d4f86c1fc46db48e31d363a40bff7f3dc6f3c7ff
rishabh-2714 <rishabh.2714@bis.edu.in>
2026-09-08 17:15:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-
+# Shaurya, please add Flask here. For mapping, here is the repository structure. Please write full paths so that we can use PythonAnywhere as a host.
+"""
+Mapping:
+/root
+│
+│——/static
+│   │
+│   │——/json/static.json
+│   │——/css/style.css
+│   │——/js/main.js
+│——/templates/index.html
+│——app.py #this file
+│——dynamic.json
+"""
